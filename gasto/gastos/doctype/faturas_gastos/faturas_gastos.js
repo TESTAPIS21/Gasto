5a5555c66b50683df581fb4f2056d03a385c062b
TESTAPIS21 <aguimashoes362@gmail.com>
2023-05-22 21:02:18 +0000

    CODE

---
+++ gasto/gastos/doctype/faturas_gastos/faturas_gastos.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Sistemas and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('faturas_gastos', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ gasto/gastos/doctype/faturas_gastos/test_faturas_gastos.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Sistemas and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testfaturas_gastos(FrappeTestCase):
+	pass
